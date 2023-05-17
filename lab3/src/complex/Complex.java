c25ca4612754c14bdce3e61d6b1866999f8532b5
Rodrigo Francisco <rodrigofrancisco@tecnico.ulisboa.pt>
2023-05-17 15:29:35 +0100

    lab3 exercises 1 and 2 completed

--- lab3/src/complex/Complex.java
+++ lab3/src/complex/Complex.java
@@ -2,6 +2,31 @@ package complex;
 
 public class Complex {
     @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + Float.floatToIntBits(a);
+        result = prime * result + Float.floatToIntBits(b);
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Complex other = (Complex) obj;
+        if (Float.floatToIntBits(a) != Float.floatToIntBits(other.a))
+            return false;
+        if (Float.floatToIntBits(b) != Float.floatToIntBits(other.b))
+            return false;
+        return true;
+    }
+
+    @Override
     public String toString() {
         return a + "+" + b + "i";
     }
--- lab3/src/main/Main.java
+++ lab3/src/main/Main.java
@@ -1,5 +1,20 @@
 package main;
 
+import complex.Complex;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        Complex n1 = new Complex(1, 1);
+        Complex n2 = new Complex(1, 1);
+
+        Complex sum = n1.add(n2);
+        Complex sub = n1.sub(n2);
+        Complex mul = n1.mult(n2);
+
+        System.out.println(sum);
+        System.out.println(sub);
+        System.out.println(mul);
+
+        System.out.println(n1.equals(n2));
+    }
 }
