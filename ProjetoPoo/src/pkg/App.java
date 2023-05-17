f025a158b302152dcb4a60fda80ff055a10cdeb5
tiagomira29 <tiagoandre.mira@gmail.com>
2023-05-17 16:55:55 +0100

    bitches

--- ProjetoPoo/src/pkg/App.java
+++ ProjetoPoo/src/pkg/App.java
@@ -1,6 +1,6 @@
 package pkg;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Grenqvist!");
+        System.out.println("Hello, Granqvist!");
     }
 }
