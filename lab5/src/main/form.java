8521a62a4a00a73ab867e2c492c76880947670f8
tiagomira29 <tiagoandre.mira@gmail.com>
2023-05-24 12:58:35 +0100

    Co-authored-by: lego.rarf2003@gmail.com <lego.rarf2003@gmail.com> Co-authored-by: jc <jcjoao@users.noreply.github.com>

---
+++ lab5/src/main/form.java
@@ -0,0 +1,8 @@
+package main;
+
+public class form {
+
+    public form() {
+    }
+    
+}
---
+++ lab5/src/main/image.java
@@ -0,0 +1,7 @@
+package main;
+
+public class image {
+    public int height;
+    public int width;
+    public form formas;
+}
